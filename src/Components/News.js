200060550e4def36e8ff263f2ccea86f7ba10c53
Shivang patel <shivangpatel83@gmail.com>
2023-04-13 02:01:59 +0530

    2nd push

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,39 +1,71 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { NewsItem } from "./NewsItem"
 
 
 export const News=()=>{
-    
-       const fetching = async ()=>{
-    const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e44c7db87b2944859a16a8fb7071c77d`
-    const getData = await fetch(url)
-    const parsedData =  await getData.json()
-    console.log(parsedData.articles)
-    return parsedData.articles
+
+        const [data, setData] = useState([])
+      const [page , setPage] = useState({current : 1, size : 0})
+      
+   useEffect( () =>  {
+     const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e44c7db87b2944859a16a8fb7071c77d`
+    fetch(url)
+     .then(response => response.json())
+     .then(parsedData=>{
+     setData(parsedData.articles)
+     setPage({...page , size : parsedData.totalResults})
+    //  console.log(parsedData.articles)
+     }
+     )
+  }, []);
+
+  const handleNextPage=()=>{
+// console.log(page.current)
+if(page.current+1>Math.ceil(page.size/20)){
+}else{
+    const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e44c7db87b2944859a16a8fb7071c77d&page=${page.current+1}&pageSize=20`
+    fetch(url)
+     .then(response => response.json())
+     .then(parsedData=>{
+     setData(parsedData.articles)
+    //  console.log(parsedData.articles)
+     }
+     )
+    setPage({...page, current : page.current+1})
+    // console.log(page)
 }
-
-// const newsArray = fetching()
-// console.log(`newsArray`, newsArray)
-
-   
-    // console.log(`hello` ,data)
-
-    const [data, setData] = useState(``)
+  }
+  const handlePrevPage=()=>{
+    console.log(page.current)
+  
+    const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e44c7db87b2944859a16a8fb7071c77d&page=${page.current-1}&pageSize=20`
+    fetch(url)
+     .then(response => response.json())
+     .then(parsedData=>{
+     setData(parsedData.articles)
+    //  console.log(parsedData.articles)
+     }
+     )
+    setPage({...page, current : page.current-1})
+    
+    // console.log(page)
+  }
 
     return (
         <>
         <div className="container my-3">
         <h3>Welcome to News WOrld! - Top Headlines</h3>
         <div className="row">
-          
-            
        { data.map((ele)=>
-    //    console.log(ele.description.slice(0,50))
               <div className="col-md-4 my-3" key = {ele.url}>
               <NewsItem  title = {ele.title} desc = {ele.description} image = {ele.urlToImage} url = {ele.url}/>
               </div>
         )}
         </div>
+        </div>
+        <div className="container d-flex justify-content-around">
+        <button disabled={page.current<=1} type="button" className="btn btn-primary" onClick={()=>{handlePrevPage()}}>&larr; Previous</button>
+        <button disabled={page.current==Math.ceil(page.size/20)} type="button" className="btn btn-primary" onClick={()=>{handleNextPage()}}>Next &rarr;</button>
         </div>
         </>
     )
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -3,10 +3,10 @@ export const NewsItem=(props)=>{
     return (
         <>
         <div className="card" style={{"width": "15rem"}}>
-  <img src= {props.image} className="card-img-top" alt="..."/>
+  <img src= {props.image?props.image:'https://images.moneycontrol.com/static-mcnews/2021/04/Delisting-770x433.jpg'} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{props.title}...</h5>
-    <p className="card-text">{props.desc}</p>
+    <p className="card-text">{props.desc?props.desc:'Read more about HEADLINE at PTI. Find out more Breaking News and Latest News Headlines from India and around the world HEADLINE word to be replaced by the headline'}</p>
     <a href={props.url} className="btn btn-primary" target="_blank">Read More</a>
   </div>
 </div>
